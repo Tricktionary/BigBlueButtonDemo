a80b507e535000c351ec895ed27355ecdb13ee01
Tricktionary <andyaoux@hotmail.com>
2019-05-01 15:17:36 -0400

    Update credentials to hit test server

--- app/helpers/init_helper.rb
+++ app/helpers/init_helper.rb
@@ -1,11 +1,13 @@
 module InitHelper
     def initialize_api
-        url = (ENV['BIGBLUEBUTTON_ENDPOINT'] || 'http://172.17.0.1/bigbluebutton/api') 
-        secret = ENV['BIGBLUEBUTTON_SECRET'] || '5d371785dc1c91c57a9115478cf82aa0'
+        url = (ENV['BIGBLUEBUTTON_ENDPOINT'] || 'http://test-install.blindsidenetworks.com/bigbluebutton/api') 
+        secret = ENV['BIGBLUEBUTTON_SECRET'] || '8cd8ef52e8e101574e400365b55e11a6'
         version = 0.81
 
         BigBlueButton::BigBlueButtonApi.new(url, secret, version.to_s, false)
     end
 end
 
-#docker run -p 80:80/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp --name bigbluebutton bigbluebutton/bigbluebutton -h 172.17.0.1
+## MY DOCKER CREDENTIALS
+## url = (ENV['BIGBLUEBUTTON_ENDPOINT'] || 'http://172.17.0.1/bigbluebutton/api') 
+## secret = ENV['BIGBLUEBUTTON_SECRET'] || '5d371785dc1c91c57a9115478cf82aa0'
