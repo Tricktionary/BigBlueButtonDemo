e27a82bc6c1284d1a0f4eb3e941f7fbe82d706f8
Tricktionary <andyaoux@hotmail.com>
2019-05-01 15:39:30 -0400

    Update circle ci

--- spec/controllers/core_controller_spec.rb
+++ spec/controllers/core_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe CoreController, type: :controller do
     
      describe "GET /" do
-        it "Render the index template successfully" do
+        it "Render the index template" do
             get :index
             expect(response.status).to render_template("index")
         end
     end
 
      describe "POST /create_and_join_meeting" do 
-        it "Redirect user successfully" do 
+        it "Redirect user after creating a meeting" do 
             params = {
                 meeting_id:"123",
                 meeting_name:"test_meeting",
@@ -23,20 +23,19 @@ RSpec.describe CoreController, type: :controller do
     end 
 
     describe "Test recording functionality" do
-        it "Get recording successfully" do
+        it "Get recordings" do
             expect(controller.get_recordings()).to_not be_nil #Get an array object back
         end 
-        it "Delete recording successfully" do
+        it "Delete recording" do
             recordings = controller.get_recordings()
             if recordings.length > 0 
                 id = recordings[0]["id"]
+                puts id
                 params={
                     recording_id: id
                 }
                 post :delete_recording, params: params
-                expect(controller.delete_recording()[:deleted]).to eql("true");
-            else
-                puts "There are currently no recordings on The BigBlueButtonServer"
+                expect(controller.delete_recording()[:deleted]).to eql('true');
             end 
         end
     end 
