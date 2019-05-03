18f931f64d6360036d1c5bf19d85c23eee20d1c0
Tricktionary <andyaoux@hotmail.com>
2019-05-03 11:20:58 -0400

    Remove recordings resource as its not needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
     
-  resources :recordings
-
   post "create_and_join_meeting" => "core#create_and_join_meeting"
   post "delete_recording" => "core#delete_recording"
 
